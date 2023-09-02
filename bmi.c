7ad34f348f9231b37238035058a617422afcab9a
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-09-02 17:10:57 +0000

    first commit for a bmi calculator using scanf

---
+++ bmi.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+
+{
+int a, b, bmi;
+printf("Type your weight in kilograms %d\n" ,a);
+printf("Type your height in centimenters %d\n" ,b);
+scanf("%d %d", &a, &b);
+bmi=a/b;
+}
