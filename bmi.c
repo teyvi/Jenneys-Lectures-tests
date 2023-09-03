75294ab1930e6515b32f85c02cd859cb62e6702c
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-09-03 22:36:20 +0000

    unformatted input example

--- bmi.c
+++ bmi.c
@@ -5,14 +5,14 @@ int main(void)
 double height_m, weight_kg, BMI;
 
 //user prompt to enter height in meters and input assigned to height_cm
-printf("Hi there!, enter your height in meters: ");
+printf("Hi there!\n enter your height in meters: ");
 scanf("%lf", &height_m);
 
 //user prompt to enter weight in kilograms and input assigned to weight_kg
 printf("Enter your weight in kilograms: ");
 scanf("%lf", &weight_kg);
 
-//calculate bmi with formular weight/(heigh 
+//calculate bmi with formular 
 BMI = weight_kg / (height_m * height_m);
 
 //print the BMI value
