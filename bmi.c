fa004805640c9a2dde2b36871209c87c7dd2230a
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-09-03 21:06:20 +0000

    addressed code error in the scanf statement

--- bmi.c
+++ bmi.c
@@ -5,16 +5,16 @@ int main(void)
 double height_m, weight_kg, BMI;
 
 //user prompt to enter height in meters and input assigned to height_cm
-printf("Hi there\n, enter your height in cm: ");
+printf("Hi there!, enter your height in meters: ");
 scanf("%lf", &height_m);
 
 //user prompt to enter weight in kilograms and input assigned to weight_kg
 printf("Enter your weight in kilograms: ");
-scanf("%lf", &weight_kg: ");
+scanf("%lf", &weight_kg);
 
 //calculate bmi with formular weight/(heigh 
 BMI = weight_kg / (height_m * height_m);
 
 //print the BMI value
-printf("Your BMI is: %.2lf\n", bmi);
+printf("Your BMI is: %.2lf\n", BMI);
 }
