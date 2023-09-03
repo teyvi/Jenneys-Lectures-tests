058dd8f531184b7d49fea618e3b3668ca175f5f2
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-09-03 21:02:06 +0000

    redefined the code to print input per prompt

--- bmi.c
+++ bmi.c
@@ -2,10 +2,19 @@
 int main(void)
 
 {
-float a, b,  bmi;
-printf("Type your weight in kilograms\n");
-printf("Type your height in centimenters");
-scanf("%f %f", &a, &b);
-bmi=b/a;
-printf("Your BMI value is: %f\n", bmi);
+double height_m, weight_kg, BMI;
+
+//user prompt to enter height in meters and input assigned to height_cm
+printf("Hi there\n, enter your height in cm: ");
+scanf("%lf", &height_m);
+
+//user prompt to enter weight in kilograms and input assigned to weight_kg
+printf("Enter your weight in kilograms: ");
+scanf("%lf", &weight_kg: ");
+
+//calculate bmi with formular weight/(heigh 
+BMI = weight_kg / (height_m * height_m);
+
+//print the BMI value
+printf("Your BMI is: %.2lf\n", bmi);
 }
