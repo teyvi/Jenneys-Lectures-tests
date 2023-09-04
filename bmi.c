b70538bbc7f6780f5f0195cb392a09def8fa14d9
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-09-04 21:47:48 +0000

    full code to check BMI

--- bmi.c
+++ bmi.c
@@ -17,4 +17,40 @@ BMI = weight_kg / (height_m * height_m);
 
 //print the BMI value
 printf("Your BMI is: %.2lf\n", BMI);
+
+//control statements for each type of obesity
+
+if (BMI < 18.5)
+{
+ printf("You are underweight\n");
+}
+
+
+else if(BMI >= 18.5 && BMI <24.5)
+{
+ printf("You are normal weight\n");
+}
+
+
+else if (BMI >= 25 && BMI <29.9)
+{
+ printf("You are overweight\n");
+}
+
+
+else if (BMI >= 30 && BMI < 34.9)
+{
+ printf("You are class I obese");
+}
+
+else if (BMI >= 35 && BMI <39.9)
+{
+ printf("You are class II obese\n");
+}
+
+else
+{
+ printf("You are morbid obese\n");
+} 
+
 }
