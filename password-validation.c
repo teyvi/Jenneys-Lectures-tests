f8af7e5ae9d050bff9240178741ed84264f20477
teyvi <44750503+teyvi@users.noreply.github.com>
2023-12-21 23:30:18 +0000

    init commit

---
+++ password-validation.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+
+{
+char lenght;
+char special-char;
+char capital-letter;
+int numbers;
+
+printf("Enter Password:")
+scanf("%c %c %c %d", &lenght, &special-char, &capital-letter, &numbers)
+
+if  
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
