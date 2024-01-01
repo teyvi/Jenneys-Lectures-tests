f4bc4428322233dafa048187cd43cc2e69ff578d
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-01 21:17:15 +0000

    removed % at the end of the result

--- switch_statement-calculator.c
+++ switch_statement-calculator.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+i#include <stdio.h>
 
 int  main()
 
@@ -8,7 +8,7 @@ int a, b, sum, div, sub,mul;
 char operator;
 
 printf("Enter your Operator:");
-scanf("%c", &operator);
+scanf(" %c", &operator);
 
 printf("Enter your first integer Operant:");
 scanf("%d", &a);
@@ -19,28 +19,26 @@ scanf("%d", &b);
 switch(operator)
 {
 	case '-': sub = a-b;
-	printf("sub = %d", sub);
+	printf("sub = %d\n", sub);
 	break;
 
 	case '+': sum = a+b;
-	printf("sum = %d", sum);
+	printf("sum = %d\n", sum);
 	break;
 	
 	case '*': mul = a*b;
-	printf("mul = %d", mul);
+	printf("mul = %d\n", mul);
 	break;
 
 	case '/': div = a/b;
 	if (b !=0)
 	{	div = a/b;
-		printf("div = %d", div;
+		printf("div = %d\n", div);
 	}
 	else{
-		printf("div = %d", div);
+		printf("div = %d"\n, div);
 		}
 	break; 
-	printf("div = %d", div);
-
 
 	default:
 	printf("Error: Invalid Operator");
