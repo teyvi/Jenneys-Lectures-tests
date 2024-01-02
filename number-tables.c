b6adffd7698f55984038488ea2a313efa4dad927
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-02 22:48:28 +0000

    number table to 12

---
+++ number-tables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{
+int i, number, result;
+printf("Enter a number:");
+scanf("%d\n", &number);
+
+for(i =0; i <=12; i ++)
+{
+	result =number *i;
+	printf("%d\n", result);
+
+}
+
+}
