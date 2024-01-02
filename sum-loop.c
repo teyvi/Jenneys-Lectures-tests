690701e6a7327827f2e593b32273bd0829884f94
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-02 23:08:29 +0000

    adding numbers between 1-100

---
+++ sum-loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+//program that finds the sum of numbers from 1-100
+
+{
+
+int i, sum;
+
+for ( i == 1; i <= 100; i ++)
+
+{ 
+sum += i;
+printf("Sum of numbers between 1 -100: %d\n", results);
+
+}
+
+return 0;
+}
