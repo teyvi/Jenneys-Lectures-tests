28e4e1bcdee3e0a9992afa9ac5655f8febb68f30
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-02 23:24:34 +0000

    shows the numbers to be added

--- sum-loop.c
+++ sum-loop.c
@@ -7,11 +7,12 @@ int main ()
 
 int i, sum;
 
-for ( i == 1; i <= 100; i ++)
+for ( i = 1; i <= 100; i ++)
 
 { 
 sum += i;
-printf("Sum of numbers between 1 -100: %d\n", results);
+
+printf("%d + %d = %d\n", i, sum - i, sum);
 
 }
 
