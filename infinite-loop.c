8e2bd9dc6d6c6993d9e78fba3c170b78e0fe1ecc
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-02 22:16:49 +0000

    infinite loop

---
+++ infinite-loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+
+{
+int i=1;
+
+for (; ;)
+{
+printf("%d\n", i);
+}
+
+return 0;
+}
--- infite-loop.c
+++ infite-loop.c
@@ -5,9 +5,9 @@ int main ()
 
 int i, j;
 
-for (i = 1, j = 0; i == 100; i ++, j ++)
+for (i = 1, j = 0; i <=  100; i ++, j ++)
 {
-	printf("%d %d", i, j);
+printf("%d %d\n", i, j);
 }
 
 	return 0;
