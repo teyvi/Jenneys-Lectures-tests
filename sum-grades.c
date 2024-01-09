7cefbbf580118660967b7382bdc018284d80c68c
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-09 13:59:26 +0000

    debugging

--- sum-grades.c
+++ sum-grades.c
@@ -4,16 +4,17 @@ void main()
 {
 
 int grade[5];
-int sum=0, avg, i;
+int sum=0, i;
+float avg;
 
-printf("Enter the grades of five students:")
+printf("Enter the grades of five students:");
 
 for(i =0; i <5; i++)
 scanf("%d", grade[i]);
 
 for (i =0; i <5; i++)
 sum = sum + grade[i];
-avg = sum/5
+avg = sum/5;
 
 printf("%f %d\n", avg, sum); 
 
