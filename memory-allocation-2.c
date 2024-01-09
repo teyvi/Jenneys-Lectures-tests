4cd4605d4b13da3fe10a12b51df63b005e173e42
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-09 12:36:24 +0000

    print element index

--- memory-allocation-2.c
+++ memory-allocation-2.c
@@ -3,10 +3,15 @@ int main()
 {
 int a[5], i;
 
+printf("Enter the array element:")
+
 for(i =0; i <5; i++)
-{
-scanf("%d", &a[i]);
-printf("%d\n" a);
-}
+
+	scanf("%d", &a[i]);
+
+for (i =0; i <5;i++)
+
+	printf("array element at index %d is: %d, i,a[i]);
+
 return 0;
 }
