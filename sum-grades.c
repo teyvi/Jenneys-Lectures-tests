ea6194600d15912e5cbeee4e3558b5363d52482b
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-09 14:32:06 +0000

    made code output readable

--- sum-grades.c
+++ sum-grades.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void main()
+int main()
 //program to calculate the grade of five students using indexing 
 {
 
@@ -10,12 +10,12 @@ float avg;
 printf("Enter the grades of five students:");
 
 for(i =0; i <5; i++)
-scanf("%d", grade[i]);
+scanf("%d", &grade[i]);
 
 for (i =0; i <5; i++)
 sum = sum + grade[i];
-avg = sum/5;
+avg = (float)sum/5;
 
-printf("%f %d\n", avg, sum); 
+printf("The average is %f and the sum is %d\n", avg, sum); 
 
 }
