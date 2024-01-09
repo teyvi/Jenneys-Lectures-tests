4f110ea103eb20d0317fa09b97bdbe39de84cbed
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-09 13:57:26 +0000

    calculate sum of grades

--- memory-allocation-2.c
+++ memory-allocation-2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-int main()
+int  main()
 {
 int a[5], i;
 
-printf("Enter the array element:")
+printf("Enter the array element:");
 
 for(i =0; i <5; i++)
 
@@ -11,7 +11,5 @@ for(i =0; i <5; i++)
 
 for (i =0; i <5;i++)
 
-	printf("array element at index %d is: %d, i,a[i]);
-
-return 0;
+	printf("\narray element at index %d is: %d", i,a[i]);
 }
---
+++ sum-grades.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+//program to calculate the grade of five students using indexing 
+{
+
+int grade[5];
+int sum=0, avg, i;
+
+printf("Enter the grades of five students:")
+
+for(i =0; i <5; i++)
+scanf("%d", grade[i]);
+
+for (i =0; i <5; i++)
+sum = sum + grade[i];
+avg = sum/5
+
+printf("%f %d\n", avg, sum); 
+
+}
