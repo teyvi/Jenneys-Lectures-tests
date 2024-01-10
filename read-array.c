2b4a240878b1d89aee4a8c1cb9a95ee7bedef6e2
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-10 13:58:26 +0000

    calculate total of odd and even

--- read-array.c
+++ read-array.c
@@ -5,6 +5,7 @@ int main()
 int i;
 int even = 0, odd = 0;
 int a[10];
+int oddSum =0 , evenSum =0;
 
 printf("Enter ten numbers:");
 for(i =0; i <10; i++)
@@ -12,16 +13,21 @@ for(i =0; i <10; i++)
 scanf("%d",&a[i]);
 
 if ( a[i] % 2 == 0)
-
+{
 	even++;
-
+	evenSum += a[i];
+}
 else
+{
 
 	odd++;
+	oddSum += a[i];
 
+}
 }
 
 printf("number of even numbers are %d", even);
 printf("\nnumber of odd numbers are %d", odd);
-
+printf("Total odd %d", oddSum);
+printf("Total even %d", evenSum);
 }
