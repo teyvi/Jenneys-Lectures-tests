0988b83e8e335e4303ceb4ab6689337a5a4f3ae8
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-11 19:14:53 +0000

    debuddeg indexing issue

--- sum-array.c
+++ sum-array.c
@@ -3,7 +3,7 @@ int main ()
 {
 
 int a [5], b[5], i;
-int sumarray[i];
+int sumarray[5];
 
 printf("Enter five numbers:");
 for (i = 0; i <5; i++)
@@ -11,13 +11,13 @@ for (i = 0; i <5; i++)
 
 printf("Enter five numbers:");
 for(i = 0; i <5; i++)
-	scanf("%d", &a[i]);
+	scanf("%d", &b[i]);
 
 for(i =0; i <5; i++)
 {
 
 	sumarray[i] = a[i] + b[i];
-	printf("\nThe sum of %d is %d:",i, sumarray[i]);
+	printf("\n The sum of %d is %d:" ,i,sumarray[i]);
 }
 
 }
