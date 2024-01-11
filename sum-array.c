dd141172498ddb278f686f0cff534c02ea4e41e0
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-11 19:09:17 +0000

    debugg

--- sum-array.c
+++ sum-array.c
@@ -3,7 +3,7 @@ int main ()
 {
 
 int a [5], b[5], i;
-float sumarray = 0;
+int sumarray[i];
 
 printf("Enter five numbers:");
 for (i = 0; i <5; i++)
@@ -15,11 +15,9 @@ for(i = 0; i <5; i++)
 
 for(i =0; i <5; i++)
 {
-	scanf("%d", &a[i]);
-	sumarray = a[i] + b[i];
+
+	sumarray[i] = a[i] + b[i];
+	printf("\nThe sum of %d is %d:",i, sumarray[i]);
 }
-
-printf("The sum of %d is %f:", a[i], sumarray);
-
 
 }
