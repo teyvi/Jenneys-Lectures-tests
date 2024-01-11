f99969b92f2cbdf9c96ddcd8a19e00195dd6fad6
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-11 18:47:56 +0000

    debugged program

---
+++ read-array-2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    int even = 0, odd = 0;
+    int a[10];
+    int oddSum = 0, evenSum = 0;
+
+    printf("Enter ten numbers:");
+
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &a[i]);
+
+        if (a[i] % 2 == 0) {
+            even++;
+            evenSum += a[i];
+        } else {
+            odd++;
+            oddSum += a[i];
+        }
+    }
+
+    printf("Number of even numbers are: %d\n", even);
+    printf("Number of odd numbers are: %d\n", odd);
+    
+    printf("Total odd: %d\n", oddSum);
+    printf("Total even: %d\n", evenSum);
+
+    return 0;
+}
+
