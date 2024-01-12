a624a3d1043f83645ec4e331c0846d0042be111d
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-12 15:56:34 +0000

    transpose metric

---
+++ transpose-array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+int i,j, a[2][3];
+
+for(i =0; i <2; i++)
+{
+	for(j =0;j <3; j++)
+	{	scanf("%d", &a[i][j]);
+	}
+}
+
+for(i =0; i <2; i++)
+{
+        for(j =0;j <3; j++)
+        {
+       printf("%d/t", a[i][j]);
+	}
+}
+
+printf("\n");
+}
