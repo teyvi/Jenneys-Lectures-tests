dac2211eb60fb43fc262da5d7e72d08e67cab99a
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-12 09:35:21 +0000

    2d array initialization

---
+++ 2d-2-array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+
+printf("Enter data pairs:");
+int a[2][3], i, j;
+
+for(i =0; i <2; i++)
+	{ for(j =0; i <3; j++)
+	  scanf("%d", &a[i][j]);
+	}
+
+return 0;
+}
