04a07cafe7bb0ba1a296ebc696b08a237b666774
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-13 22:13:58 +0000

    day over

--- .vscode/rows-col.c
+++ .vscode/rows-col.c
@@ -10,7 +10,7 @@ for(i = 0; i <3; i++)
 {
 	for (j =0; j <3; j++)
 {
-	scanf("%d", &a[i][j];
+	scanf("%d", &a[i][j]);
 
 }
 
@@ -30,4 +30,5 @@ printf("\nSum of row= %d, sum of column = %d", sumrow, sumcol);
 
 }
 
+}
 }
