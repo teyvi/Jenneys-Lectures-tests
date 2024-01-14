baba276b6f0fbad539f8aa00b3032b231d271965
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-14 19:48:41 +0000

    string lenght

--- string-len.c
+++ string-len.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
+int main() {
+    char name[30];
 
-char name[30];
-printf("Enter your name:\n");
-gets(name);
-strlen(name);
+    printf("Enter your name:\n");
+    fgets(name, sizeof(name), stdin);  // Read the name using fgets
 
+    // Remove the newline character at the end, if present
+    if (name[strlen(name) - 1] == '\n') {
+        name[strlen(name) - 1] = '\0';
+    }
+
+    printf("Your name is: %s\n", name);
+    printf("Length of your name is: %lu\n", strlen(name));
+
+    return 0;
 }
+
