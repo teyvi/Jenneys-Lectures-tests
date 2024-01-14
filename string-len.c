574d7ca9fedbe1c5e5d64e033f0563a1d1c190e4
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-14 20:31:42 +0000

    added while loop

--- string-len.c
+++ string-len.c
@@ -3,17 +3,23 @@
 
 int main() {
     char name[30];
+    int count=0, i =0;
 
     printf("Enter your name:\n");
     fgets(name, sizeof(name), stdin);  // Read the name using fgets
 
-    // Remove the newline character at the end, if present
-    if (name[strlen(name) - 1] == '\n') {
-        name[strlen(name) - 1] = '\0';
-    }
+    // count the strings
+    while (name[i] !='\0')
+{
+	count++;
+	i++;
+
+
+
+}
 
     printf("Your name is: %s\n", name);
-    printf("Length of your name is: %lu\n", strlen(name));
+    printf("Length of your name is: %d\n",  count);
 
     return 0;
 }
