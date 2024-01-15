52bcefa0e5da19e8b0d7a7b222fe5cdbde669354
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-15 09:21:07 +0000

    using my  string concatonation function

--- stringcat.c
+++ stringcat.c
@@ -1,11 +1,22 @@
 #include<stdio.h>
-#include<sting.h>
+#include<string.h>
 int main()
 {
 
+int l1,l2,i;
 char s1[30]= "Angela";
 char s2[20]= "Teyvi";
-strcat(s1,s2);
+//strcat(s1,s2);
+
+l1=strlen(s1);
+l2=strlen(s2);
+for(i=0; i <=l2; i++)
+{
+s1[l1+i]=s2[i];
+
+}
+
+
 printf("string after concatonation is %s:\n", s1);
 puts(s2);
 
