92f926e18718bafa5a3e50e326deb7cb4dee688e
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-15 09:11:59 +0000

    concatonate strings using strcat

---
+++ stringcat.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<sting.h>
+int main()
+{
+
+char s1[30]= "Angela";
+char s2[20]= "Teyvi";
+strcat(s1,s2);
+printf("string after concatonation is %s:\n", s1);
+puts(s2);
+
+}
