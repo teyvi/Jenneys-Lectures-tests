f21b3b3f5c12fb97db8d5750c4139d81225e4928
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-22 16:21:30 +0000

    debugging syntax

---
+++ password/function-defination.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+//declare prototype
+void Sum();
+
+int main()
+
+{
+Sum();
+return 0;
+}
+
+void Sum()
+{
+int a,b;
+int sum = 0;
+printf("Enter two numbers:\n");
+scanf("%d %d",&a,&b);
+sum = a + b;
+printf("Sum of the numbers are %d:", sum);
+
+}
