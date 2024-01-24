50c9957568c82c1c0be48229fcb2292978857aa7
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-24 20:28:53 +0000

    added substraction section to code

--- password/no_argument-function.c
+++ password/no_argument-function.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 void sum();
+void sub();
 int main()
 
 {
 sum();
+sub();
 }
 
 void sum()
@@ -15,4 +17,16 @@ printf("Enter your second integer:\n");
 scanf("%d", &b);
 Sum= a + b;
 printf("Sum is %d\n",Sum);
+}
+
+void sub()
+{
+int a, b, Sub=0;
+printf("Enter your first integer:\n");
+scanf("%d", &a);
+printf("Enter your second integer:\n");
+scanf("%d", &b);
+Sub= a - b;
+printf("Substraction is %d\n",Sub);
+
 }
