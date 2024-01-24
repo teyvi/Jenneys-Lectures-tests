1767c4a4c19b4f27359f60341ec91e65a655d5ab
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-24 20:04:25 +0000

    dubugging

--- password/function-call-pointers.c
+++ password/function-call-pointers.c
@@ -1,17 +1,15 @@
- 1 #include<stdio.h>
-  2 
-  3 void fun(int,int);
-  4 int main();
-  5 
-  6 {
-  7         int x= 5, y= 7;
-  8         fun(&x,&y);
-  9         printf("x= %d, y= %d\n", x,y);
- 10 }
- 11 
- 12 void fun(int *x, int *y)
- 13 {
- 14         *x= 7;
- 15         *y= 5;
- 16         printf("x= %d, y= %d", *x,*y);
- 17 }
+ #include<stdio.h>
+ 
+ void fun(int *, int*);
+ int main() 
+ {
+         int x= 5, y= 7;
+         fun(&x,&y);
+         printf("x= %d, y= %d\n", x,y);
+ } 
+ void fun(int *x, int *y)
+{
+         *x= 7;
+         *y= 5;
+         printf("x= %d, y= %d", *x,*y);
+ }
