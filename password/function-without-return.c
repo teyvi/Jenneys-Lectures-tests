b57dccbaa1f3a8abd7cd08d9c157325743aa4ace
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-25 08:30:49 +0000

    function with no return but argument

---
+++ password/function-without-return.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void sum(float, float);
+int main()
+{
+	float x, y;
+	printf("Enter two float numbers\n");
+	scanf("%f %f", &x, &y);
+	sum(x,y);
+}
+
+void sum(float x, float y)
+{
+	float Sum= 0;
+	Sum= x + y;
+}
