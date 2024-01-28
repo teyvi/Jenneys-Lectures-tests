07cdd7636b1a2c8f082c463d71cb56098f0bd2a3
teyvi <44750503+teyvi@users.noreply.github.com>
2024-01-28 19:38:33 +0000

    switch statement and function pointer comparison

---
+++ c-projects/simple-calculator.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+// Function prototypes
+void sum(int a, int b);
+void sub(int a, int b);
+void mul(int a, int b);
+void div(int a, int b);
+
+void sum(int a, int b)
+{
+    printf("%d\n", a + b);
+}
+
+void sub(int a, int b)
+{
+    printf("%d\n", a - b);
+}
+
+void mul(int a, int b)
+{
+    printf("%d\n", a * b);
+}
+
+void div(int a, int b)
+{
+    printf("%d\n", a / b);
+}
+0
+int main()
+{
+    int choice = 0, a, b;
+
+    printf("Enter your arithmetic choice: \n0 for addition\n1 for subtraction\n2 for multiplication\n3 for division\n");
+    printf("Enter: ");
+    scanf("%d", &choice);
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    switch (choice)
+    {
+    case 0: sum(a, b); break;
+    case 1: sub(a, b); break;
+    case 2: mul(a, b); break;
+    case 3: div(a, b); break;
+    default: printf("Enter a valid choice");
+    }
+
+    return 0;
+}
